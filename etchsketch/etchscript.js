9fead1edfa7f8a223ee843fb1b0020c993925711
rxthew <richardtheumaster@gmail.com>
2021-08-26 05:09:41 +0200

    some updates

--- etchsketch/etchscript.js
+++ etchsketch/etchscript.js
@@ -1,6 +1,16 @@
-for (let i = 0; i <= 5; i++){
+let container = document.createElement('div');
+container.setAttribute('id', 'container');
+document.body.appendChild(container);
+container = document.querySelector('#container');
+
+for (let i = 0; i <= 9999; i++){
     let div = document.createElement('div');
-    document.body.appendChild(div)  
+    div.addEventListener('mouseover', function(e){
+        console.log(e.target);
+        e.target.style.backgroundColor = 'white';
+    }, {once:true});
+    container.appendChild(div)  
  };
+
 
 
