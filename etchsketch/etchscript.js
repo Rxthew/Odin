98450924a9be6fad145527a49187559760f14881
rxthew <richardtheumaster@gmail.com>
2021-08-27 01:31:23 +0200

    Rough etch-a-sketch finalised.

--- etchsketch/etchscript.js
+++ etchsketch/etchscript.js
@@ -1,14 +1,35 @@
 let container = document.createElement('div');
 container.setAttribute('id', 'container');
 document.body.appendChild(container);
+
 container = document.querySelector('#container');
+let grids; 
+let bttn = document.createElement('button');
+bttn.setAttribute('id','erase');
+bttn.textContent = 'Reset';
+bttn.addEventListener('click', function(){
+    let whites = document.querySelectorAll('.white');
+    for (let x = 0; x < whites.length;x++){
+        whites[x].classList.toggle('white', false)
+    }
+    grids = prompt('Choose your grid dimensions. The higher the number the thinner the brush');
+    if (grids > 100 | grids < 1){grids = 100};
+    container.style.gridTemplateColumns = `repeat(${grids}, 1fr)`;
+    container.style.gridTemplateRows =  `repeat(${grids},1fr)`;
+
+})
+
+let header = document.querySelector('#header')
+header.appendChild(bttn);
 
 for (let i = 0; i <= 9999; i++){
     let div = document.createElement('div');
     div.addEventListener('mouseover', function(e){
-        console.log(e.target);
-        e.target.style.backgroundColor = 'white';
-    }, {once:true});
+        e.target.classList.toggle('white', true);
+    });
+    div.addEventListener('click', function(e){
+        e.target.classList.toggle('white', false);
+    })   
     container.appendChild(div)  
  };
 
