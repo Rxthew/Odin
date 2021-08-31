10e85b7d3590de1c597ac74418e275b62cc9565b
rxthew <richardtheumaster@gmail.com>
2021-08-31 21:41:34 +0200

    FInal tweaks

--- rockpapscissors/script.js
+++ rockpapscissors/script.js
@@ -11,22 +11,22 @@ let playRound = function playRound(playerSelection){
     let computerSelection = computerPlay();
     if (computerSelection == 0 && playerSelection.target.textContent=="Paper"){
         alert('You Win! Paper beats Rock.')
-        return 'You Win! Paper beats Rock.'}
+        return 'Win'}
     else if (playerSelection.target.textContent=="Rock" && computerSelection == 1){
         alert('You Lose! Paper beats Rock.')
-        return 'You Lose! Paper beats Rock.'}
+        return 'Lose'}
     else if (computerSelection == 0 && playerSelection.target.textContent=="Scissors"){
         alert('You Lose! Rock beats Scissors.')
-        return 'You Lose! Rock beats Scissors.'} 
+        return 'Lose'} 
     else if (playerSelection.target.textContent=="Rock" && computerSelection == 2){
         alert('You Win! Rock beats Scissors.')
-        return 'You Win! Rock beats Scissors.'} 
+        return 'Win'} 
     else if (computerSelection == 1 && playerSelection.target.textContent=="Scissors"){
         alert('You Win! Scissors beats Paper.')
-        return 'You Win! Scissors beats Paper.'}
+        return 'Win'}
     else if (playerSelection.target.textContent=="Paper" && computerSelection == 2){
         alert('You Lose! Scissors beats Paper.')
-        return 'You Lose! Scissors beats Paper.'}
+        return 'Lose'}
     else {alert('It\'\s a Draw! Try again.')
         return 'It\'\s a Draw! Try again.'}
 
@@ -36,14 +36,20 @@ let playRound = function playRound(playerSelection){
 function Game(playerSelection){
     let computerScore = document.querySelector('#compscr');
     let yourScore = document.querySelector('#yourscr');
+
     if (parseInt(yourScore.textContent) <= 4 && parseInt(computerScore.textContent) <=4 && parseInt(computerScore.textContent) + parseInt(yourScore.textContent) < 10){
     let p = playRound(playerSelection);
-    if(p.match(/Win/)){yourScore.textContent = parseInt(yourScore.textContent) + 1;}
-    if(p.match(/Lose/)){computerScore.textContent = parseInt(computerScore.textContent) + 1;}
+    if(p.match(/Win/)){yourScore.textContent = parseInt(yourScore.textContent) + 1;
+                       if (yourScore.textContent == 5){
+                       alert('Congratulations! You Win!');
+                       window.location.reload()}}
+    if(p.match(/Lose/)){computerScore.textContent = parseInt(computerScore.textContent) + 1;
+                       if (computerScore.textContent == 5){
+                       alert('You lose this time. Better luck next time!');
+                       window.location.reload()}}
     
-    }
-    else {alert('Game over');
-        window.location.reload()}
+}
+
     
 }
 
