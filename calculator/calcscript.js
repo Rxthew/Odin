b9ff43a898bb68b50bac6c40fc98d8901a5b8cab
rxthew <richardtheumaster@gmail.com>
2021-09-01 04:14:11 +0200

    Major updates incl. most of the logic.

--- calculator/calcscript.js
+++ calculator/calcscript.js
@@ -3,7 +3,7 @@ function add (a,b){
 };
 
 function subtract(a,b){
-    return a - b;
+    return(b > a ? 0 : a - b)
 }
 
 function multiply(a,b){
@@ -11,7 +11,7 @@ function multiply(a,b){
 }
 
 function divide (a,b){
-    return a / b;
+    return (b == 0 ? 0 : a / b)
 }
 
 function operate(a,b,operator){
@@ -20,10 +20,62 @@ function operate(a,b,operator){
             return add(a,b);
         case '-':
             return subtract(a,b);
-        case '*':
+        case String.fromCharCode(215):
             return multiply(a,b);
-        case '/':
+        case String.fromCharCode(247):
             return divide(a,b);
     }
 }
+
+const input = document.querySelector('#input');
+let userInputs = Array.from(document.querySelectorAll('.numbtn'));
+let userOperates = Array.from(document.querySelectorAll('.operate'));
+const Operand = {};
+Operand.first = 0;
+Operand.second = 0;
+Operand.operator = '';
+
+userInputs = userInputs.map(function(inp){return inp.addEventListener('click',function(e){
+    if (Operand.first == 0){
+        if (input.textContent.toString().replace(/\s+/g, '').length == 1 && input.textContent.toString().replace(/\s+/g, '') == 0){
+        input.textContent = e.target.textContent;
+        console.log(Operand);
+}
+        else {input.textContent = input.textContent.toString() + e.target.textContent.toString()
+}}
+    else if (isNaN(+(input.textContent))){
+        input.textContent = e.target.textContent.toString();
+        Operand.second = input.textContent;
+
+}
+    else if (input.textContent != 0){
+          input.textContent = input.textContent.toString() + e.target.textContent.toString();
+          Operand.second = input.textContent;
+
+}
+    
+    else {
+        Operand.first = 0;
+}
+
+})})
+
+userOperates = userOperates.map(function(op){return op.addEventListener('click', function(e){
+        if (input.textContent != 0){
+             if (Operand.second == 0){
+                  Operand.first = input.textContent;
+                  input.textContent = e.target.textContent; 
+                  Operand.operator = input.textContent;
+                }
+            else {
+                Operand.first = operate((+Operand.first),(+Operand.second), Operand.operator);
+                Operand.second = 0;
+                input.textContent = e.target.textContent;
+                Operand.operator = input.textContent;
+                console.log(Operand)
+            }
+    }
+} )})
+
+
 
