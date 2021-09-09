9ceac42d9e9983be765c9915f5d43484e45ebdea
rxthew <richardtheumaster@gmail.com>
2021-09-09 04:31:23 +0200

    Updates

--- library/libscript.js
+++ library/libscript.js
@@ -1,41 +1,55 @@
-let myLib = ['A Tale of Two Cities', 'Pride and Prejudice', 'War and Peace', '1984'];
-
+let myLib = [];
 let span;
+let div;
 
-function Book (title, author='an unidentified author', pageQuant='with an undetermined length of', readStatus='unread'){
+function Book(title, author='unknown', pageQuant='unknown',readStatus='unread'){
     this.title = title;
     this.author = author;
-    this.pageQuant = pageQuant;
-    this.readStatus = readStatus;
+    
 
-    Book.prototype.info =  function(){
-        return `${title} by ${author}, ${pageQuant} pages, ${readStatus}`
-    }
+    Book.prototype.pageChecker = function pageChecker(){
+        return (isNaN(+(pageQuant)) ? 'unknown' : pageQuant);
+     };
+    Book.prototype.readStatus = readStatus;
+
+    this.pageQuant = this.pageChecker();
 }
 
 
-myLib.forEach(function(i){
-    id = myLib.indexOf(i).toString();
-    span = document.createElement('span');
-    span.textContent = i;
-    span.setAttribute('class', 'book');
-    span.setAttribute('id', `${id}`)
-    document.querySelector('#library').appendChild(span);
-})
 
-let addButton = document.querySelector('#new')
+const addButton = document.querySelector('#new')
+const form = document.querySelector('#formcontainer');;
+const library = document.querySelector('#library');
+
 addButton.addEventListener('click',function(){
-    const form = document.querySelector('#formcontainer');
-    const library = document.querySelector('#library');
     if (form.style.visibility != 'visible'){
     library.style.opacity = '0.2';
     form.style.visibility = 'visible';
     } 
 })
 
+const submitBook = document.querySelector('#submit');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readingInput = document.querySelector('#readstatus');
 
-function addBookToLibrary(){
-//still need to fill in.//
 
-}
+submitBook.addEventListener('click', function(){
+    let details = new Book(titleInput.value, authorInput.value, pagesInput.value, readingInput.value);
+    myLib.push(details);
+    let id = (myLib.length -1 ).toString();
+    div = document.createElement('div');
+    div.setAttribute('class', 'book');
+    div.setAttribute('id', `${id}`);
+    document.querySelector('#library').appendChild(div);
+    Object.values(details).forEach(function(i){
+        span = document.createElement('span');
+        span.textContent = i;
+        span.setAttribute('id', `${i}`);
+        div.appendChild(span);
+    })
+    form.style.visibility = 'hidden';
+    library.style.opacity = '1';
+})
 
