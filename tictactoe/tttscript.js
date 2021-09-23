d449ce20836d933c18e36c663b4ce00f4e93ae22
rxthew <richardtheumaster@gmail.com>
2021-09-23 03:28:54 +0200

    Some initial things

---
+++ tictactoe/tttscript.js
@@ -0,0 +1,40 @@
+
+const game = (function(){
+    const board = (function(){
+        let gameBoard = [['','',''],
+                         ['','',''],
+                         ['','','']];
+
+        const container = document.createElement('main');
+        container.setAttribute('id','container');
+        document.body.appendChild(container);
+
+        gameBoard.forEach(
+            function(outelem){
+                let row = document.createElement('div');
+                row.setAttribute('class','row');
+                row.setAttribute('id', `row${gameBoard.indexOf(outelem)}`)
+                container.appendChild(row);
+
+                outelem.forEach(elem){
+                    let block = document.createElement('span');
+                    block.setAttribute('id', `${gameBoard.indexOf(outelem)}+${outelem.indexOf(elem)}`);
+                    row.appendChild(block);
+                }
+            }
+        )
+
+        return gameBoard
+    }());
+    
+    const player = function(symbol){
+        return {symbol}
+    };
+
+    const choosePlayer = function(){}
+
+    return {board, 'player1': player(player1), 'player2': player(player2)};
+})()
+
+
+console.log(game);
