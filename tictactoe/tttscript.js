95760697397e5fb1d0dfed41b0a6f200c43b9c1d
rxthew <richardtheumaster@gmail.com>
2021-10-01 02:07:51 +0200

    Fixed update to gameBoard. tweaked initDOM & more

--- tictactoe/tttscript.js
+++ tictactoe/tttscript.js
@@ -3,26 +3,49 @@ const game = (function(){
 
     let container = document.createElement('main');
     container.setAttribute('id','container');
-    document.body.appendChild(container);;
+    document.body.appendChild(container);
+    let current = [];
+
+    const currentPlayer = function(current){//STILL NEED TO TEST THIS.
+        if (current === []){ 
+            current.push(game.player1)
+            return current[0];
+    }
+        else if (current[0] === game.player1){
+            current[0] = game.player2;
+            return current[0];
+        }
+
+        else {current[0] = game.player1;
+              return current[0];
+    }
+};
+    const populateBlock = function(e){
+        game.board.gameBoard[this.id[0]][this.id[1]] = currentPlayer(current);
+        e.target.textContent = currentPlayer(current);
+    }
     
     const initDOMBoard = function(gameBoard){
          gameBoard.forEach(
-            function(outerElem){
+            function(outerElem, outIndex){
                 let row = document.createElement('div');
                 row.setAttribute('class','row');
-                row.setAttribute('id', `row${gameBoard.indexOf(outerElem)}`);
+                row.setAttribute('id', `row${outIndex}`);
                 container.appendChild(row);
     
                 outerElem.forEach(
-                    function(elem){
+                    function(elem, elemIndex){
                     let block = document.createElement('span');
                     block.setAttribute('class', 'block');
-                    block.setAttribute('id', `${gameBoard.indexOf(outerElem)}${outerElem.indexOf(elem)}`);
+                    block.setAttribute('id', `${outIndex}${elemIndex}`);
+                    block.addEventListener('click', populateBlock);
                     row.appendChild(block);
                     })
                 }
             )   
         }
+
+    
     
     const board = (function(){
         let gameBoard = [['','',''],
@@ -31,7 +54,7 @@ const game = (function(){
 
         initDOMBoard(gameBoard);
 
-        return gameBoard
+        return {gameBoard}
     }());
 
     const choosePlayer = (function(){
@@ -39,15 +62,13 @@ const game = (function(){
         const form = document.querySelector('#form');
         let player1 = document.querySelector('#O');
         let player2 = document.querySelector('#X');
-
+        player1.checked = true;
 
         const formInputSelect = function(){
             if (player2.checked){
- //               player2.checked = true;
- //               player1.checked = false; Still need to solve this.
                 [player1, player2] = [player2, player1];
+                [game.player1, game.player2] = [player1.value,player2.value];
             }
-            return {player1, player2}
             
         }
 
@@ -61,21 +82,14 @@ const game = (function(){
 
         form.addEventListener('submit', function(e){
             e.preventDefault();
-            formInputSelect();
+            formInputSelect()
             hideForm(form);
             revealContainer(container);
-            
         })
-
-        
         return {player1: player1.value, player2: player2.value}
 
     })()
-
     return {
         board, 'player1': choosePlayer.player1, 'player2': choosePlayer.player2
 };
 })()
-
-
-
