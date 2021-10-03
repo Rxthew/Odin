55fd910e183c24da61db260c1fc39b3cded1210e
rxthew <richardtheumaster@gmail.com>
2021-10-04 01:27:57 +0200

    Spaghetti Code => Spaghetti Class 'refactoring' ;)

--- library/libscript.js
+++ library/libscript.js
@@ -1,106 +1,115 @@
-let myLib = [];
-let span;
-let div;
 
-function Book(title, author, pageQuant,readStatus){
-    this.Title = title;
-    this.Author = author;
-    this.readStatus = readStatus;
-    function pageChecker(){
-        return (isNaN(+(pageQuant)) ? 'unknown' : pageQuant);
+class Book{
+    constructor(title, author, pageQuant,readStatus){
+        this.Title = title;
+        this.Author = author;
+        this.readStatus = readStatus;
+        function pageChecker(){
+            return (isNaN(+(pageQuant)) ? 'unknown' : pageQuant);
      };
-    this.Pages = pageChecker();
+        this.Pages = pageChecker();
+}}
+
+
+class Library{
+    
+    static myLib = [];
+    static form = document.querySelector('#formcontainer');
+    static library = document.querySelector('#library');
+    static addButton = document.querySelector('#new');
+    static titleInput = document.querySelector('#title');
+    static authorInput = document.querySelector('#author');
+    static pagesInput = document.querySelector('#pages');
+    static readingInput = document.querySelector('#readstatus');
+
+
+    static enableShowForm(){  
+      return  Library.addButton.addEventListener('click', function(){  
+        if (Library.form.style.visibility != 'visible'){
+        Library.library.style.opacity = '0.2';
+        Library.form.style.visibility = 'visible';
+        }})
+    }
+
+    static enableSubmitBook(){
+          return  Library.form.addEventListener('submit', function(e){
+            e.preventDefault();
+            let details = new Book(Library.titleInput.value, Library.authorInput.value, Library.pagesInput.value, Library.readingInput.value)
+        
+            
+            function duplicateWarning(){
+                Library.myLib.forEach(function(i){
+                    if (i.Title == details.Title && i.Author == details.Author){
+                        return alert('You seem to have added this book already.')
+                    }
+                })
+            }
+            duplicateWarning();
+            Library.myLib.push(details);
+            let id = (Library.myLib.length -1 ).toString();
+        
+        
+        
+            //Setting up book's container
+            let div = document.createElement('div');
+            div.setAttribute('id', `${id}`);
+            div.setAttribute('class', 'bookCont');
+            document.querySelector('#library').appendChild(div);
+        
+            //Remove button.
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'X';
+            removeButton.setAttribute('class', 'remove')
+            removeButton.addEventListener('click', function(e){
+                let correspondingDetails = +(e.target.parentNode.id);
+                Library.myLib[correspondingDetails].removed = true;
+                e.target.parentNode.remove();
+            })
+            div.appendChild(removeButton);     
+        
+            //Main display of the book.
+            let span = document.createElement('span');
+            Object.keys(details).forEach(function(i){
+                if (i != 'readStatus'){
+                let p = document.createElement('p');
+                details[i].length > 1 ? details[i] : details[i] = 'unknown';
+                p.textContent = `${i}: ${details[i]}` ;
+                span.appendChild(p)
+            }
+            })
+            
+            span.setAttribute('id', `${details.title}`)
+            span.setAttribute('class', 'book');
+            div.appendChild(span)
+        
+            //Read Status display, including functionality/
+            const readbtn = document.createElement('button');
+            readbtn.setAttribute('class', 'status')
+            readbtn.textContent = details.readStatus;
+            readbtn.addEventListener('click', function(e){
+                e.target.textContent == 'Unread' ? e.target.textContent = 'Read' : e.target.textContent = 'Unread';
+                let correspondingDetails = +(e.target.parentNode.id);
+                Library.myLib[correspondingDetails].readStatus = e.target.textContent
+             })
+            div.appendChild(readbtn);    
+             
+        
+            
+            //Reverting form to default state 
+        
+            Library.form.style.visibility = 'hidden';
+            Library.library.style.opacity = '1';
+            Library.titleInput.value = '';
+            Library.authorInput.value = '';
+            Library.pagesInput.value = '';
+            
+        })
+
+    }
+
+
 }
 
-const addButton = document.querySelector('#new')
-const form = document.querySelector('#formcontainer');;
-const library = document.querySelector('#library');
-
-addButton.addEventListener('click',function(){
-    if (form.style.visibility != 'visible'){
-    library.style.opacity = '0.2';
-    form.style.visibility = 'visible';
-    } 
-})
-
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const pagesInput = document.querySelector('#pages');
-const readingInput = document.querySelector('#readstatus');
-
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    let details = new Book(titleInput.value, authorInput.value, pagesInput.value, readingInput.value)
-
-    
-    function duplicateWarning(myLib, details){
-        myLib.forEach(function(i){
-            console.log(myLib);
-            if (i.Title == details.Title && i.Author == details.Author){
-                return alert('You seem to have added this book already.')
-            }
-        })
-    }
-    duplicateWarning(myLib,details);
-    myLib.push(details);
-    let id = (myLib.length -1 ).toString();
-
-
-
-    //Setting up book's container
-    div = document.createElement('div');
-    div.setAttribute('id', `${id}`);
-    div.setAttribute('class', 'bookCont');
-    document.querySelector('#library').appendChild(div);
-
-    //Remove button.
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'X';
-    removeButton.setAttribute('class', 'remove')
-    removeButton.addEventListener('click', function(e){
-        correspondingDetails = +(e.target.parentNode.id);
-        myLib[correspondingDetails].removed = true;
-        e.target.parentNode.remove();
-    })
-    div.appendChild(removeButton);     
-
-    //Main display of the book.
-    span = document.createElement('span');
-    Object.keys(details).forEach(function(i){
-        if (i != 'readStatus'){
-        let p = document.createElement('p');
-        details[i].length > 1 ? details[i] : details[i] = 'unknown';
-        p.textContent = `${i}: ${details[i]}` ;
-        span.appendChild(p)
-    }
-    })
-    
-    span.setAttribute('id', `${details.title}`)
-    span.setAttribute('class', 'book');
-    div.appendChild(span)
-
-    //Read Status display, including functionality/
-    const readbtn = document.createElement('button');
-    readbtn.setAttribute('class', 'status')
-    readbtn.textContent = details.readStatus;
-    readbtn.addEventListener('click', function(e){
-        e.target.textContent == 'Unread' ? e.target.textContent = 'Read' : e.target.textContent = 'Unread';
-        correspondingDetails = +(e.target.parentNode.id);
-        myLib[correspondingDetails].readStatus = e.target.textContent
-     })
-    div.appendChild(readbtn);    
-     
-
-    
-    //Reverting form to default state 
-
-    form.style.visibility = 'hidden';
-    library.style.opacity = '1';
-    titleInput.value = '';
-    authorInput.value = '';
-    pagesInput.value = '';
-    
-})
-
+Library.enableShowForm();
+Library.enableSubmitBook();
 
