af263ae6f6829dbf0c0d68dc5aff4362dd63871d
rxthew <richardtheumaster@gmail.com>
2021-10-05 16:22:36 +0200

    Webpack setup

--- restaurant/webpack.config.js
+++ restaurant/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    plugins: [
+       new htmlWebpackPlugin({
+           title: 'restaurant',
+       })
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
