040c174fd411792829a675c35c1c120065359c6f
rxthew <richardtheumaster@gmail.com>
2021-10-09 20:08:18 +0200

    Created abstract factory type

---
+++ todo/src/base.js
@@ -0,0 +1,78 @@
+export const baseCreate = function(storage){
+      
+const _storage = storage || new Array();
+
+(function(){
+    if (!Array.isArray(_storage)){
+        throw 'Error! Argument must be an array';
+    }
+    })()
+    
+const add = function(elem){
+    _storage.push(elem);
+
+};
+
+const remove = function(elem, index=0){
+    if (index === 0){
+        for (let i = index; i < _storage.length; i++){
+            if(_storage[i] === elem){
+                _storage.splice(i,1);
+                break;
+           }
+         }         
+       }
+       else if (index > 0 && index < _storage.length){
+           _storage.splice(index, 1)
+       }  
+};
+
+const updateByLocation = function(index, replace){
+        if (index > 0 && index < _storage.length){
+            _storage.splice(index,1,replace);
+        }
+
+};
+
+const updateByElement = function(elem, replace){
+        for (let x = index; x < _storage.length; x++){
+            if(_storage[x] === elem){
+                _storage.splice(x,1, replace);
+                break;
+            }}
+};
+
+
+const move = function(index, target){
+    if (index >= 0 && index <_storage.length && target >= 0 && target <_storage.length){
+        const moved = _storage[index];
+        _storage.splice(index,1);
+        _storage.splice(target,0,moved)
+    }       
+};
+
+const swap = function(indexA, indexB){
+    if (indexA >= 0 && indexA <_storage.length && indexB >= 0 && indexB <_storage.length){
+        
+       [_storage[indexA], _storage[indexB]] = [_storage[indexB], _storage[indexA]]
+    }
+
+};
+
+const copy = function (index){
+    if (index >= 0 && index < _storage.length){
+        let clone = _storage[index];
+        _storage.push(clone);
+    }
+}
+    
+    return {
+        add,
+        remove,
+        updateByLocation,
+        updateByElement,
+        move,
+        swap,
+        copy
+    }   
+}
