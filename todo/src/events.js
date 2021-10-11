822228401e1be78a181ff64993b8afab094b6cee
rxthew <richardtheumaster@gmail.com>
2021-10-11 03:02:48 +0200

    Tinkering with events module.

--- todo/src/events.js
+++ todo/src/events.js
@@ -1,8 +1,54 @@
+const eventsMod = function(){
 
-const eventOperators = function(e){
-    const eventHandlers = []
+    myEvents = {}
 
+    const events = function(name){
+
+        name = name;
+
+        (function(){
+            myEvents[name] = [];
+        })()
+
+        const addHandler = function(){
+
+        };
+
+        const removeHandler = function(){
+
+        };
+
+        const fireMe = function(){
+
+        };
+
+    //const eventsManager = function(){}---from here onward nested in this (except the return, gotta move that up)       
+          
+        const getMyEvent = function(anEvent){
+    
+        }; 
+       
+        const publish = function(){
+            let yourEvent = getMyEvent(anEvent);
+            yourEvent.fireMe()
+    
+        };
+
+        const  subscribe = function(){
+            let yourEvent = getMyEvent(anEvent);
+            yourEvent.addHandler(newHandler)
+        }; 
+            
+        return {
+            addHandler,
+            removeHandler,
+            fireMe,
+        }
+        
+    };
+
+
+    
+    
 }
 
-const eventCtrl = function (){ 
-}
