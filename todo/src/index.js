d6973562a61654893a73841c55aa7c7151dc209a
rxthew <richardtheumaster@gmail.com>
2021-10-13 23:47:29 +0200

    rendering some constant elements

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,14 +3,27 @@ import { DOMGeneral } from "./helpers/DOM";
 
 
 
-const createContainer = (function(){
+const revealToDo = (function(){ 
+
     const DOM = DOMGeneral();
-    const container =  DOM.elementInit('div',{'id':'container'})
-    document.body.appendChild(container)
-    return container
+
+    const mainContainer = (function(){
+    
+        const container =  DOM.elementInit('div',{'id':'container'})
+        document.body.appendChild(container)
+        return container
      
 })()
 
+    const header = (function(){
+        const head = DOM.elementInit('header', {'id':'header'});
+        const title = DOM.elementInit('h1',{'id': 'title'}, 'This is your To-Do list');
+        head.appendChild(title);
+        mainContainer.appendChild(head);
+        return head;
+})()
 
-console.log(createContainer.container)
 
+})()
+
+    
