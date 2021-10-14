323224b05c7e5c7ff39250751d3fb66c8ba18054
rxthew <richardtheumaster@gmail.com>
2021-10-14 18:16:52 +0200

    createProject function

--- todo/src/projectsDOM.js
+++ todo/src/projectsDOM.js
@@ -2,10 +2,27 @@ import { DOMGeneral } from "./helpers/DOM";
 import { projectEvents } from "./projectevents";
 
 
-const createProject = function(){
+export const templateDOMStructs = function (){
 
-}
+    const DOM = DOMGeneral()
 
-const createToDoNote = function (){
+    const createProject = function(name){
+         const reference = DOM.selectElem('.project'); 
+         const proj = DOM.elementInit('div',{'id': `${name}${reference.length}`,
+                                             'class': 'project'},name
+                                             )
+        const container = DOM.selectElem('#container');
+        container.appendChild(proj);
+        return proj
 
+     }
+
+     const createToDoNote = function (){
+
+     }
+
+     return {
+         createProject,
+         createToDoNote
+     }
 }
