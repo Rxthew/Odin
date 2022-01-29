cb7faa2dbad1075ac75c26af99e405cfd86d13c4
rxthew <richardtheumaster@gmail.com>
2022-01-29 20:27:30 +0100

    Caesar implementation (ugly)

--- practise/testPractice/caesar.js
+++ practise/testPractice/caesar.js
@@ -1,3 +1,41 @@
-export const caesar = function(){
+export const caesar = function(someString, shift){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    
+
+    if(typeof someString === 'string' && typeof shift === 'number'){
+        
+        
+        let strArray = someString.split('')
+        for(let indx = 0; indx < strArray.length; indx++){
+            let referenceIndex = alphabet.indexOf(strArray[indx].toLowerCase()) 
+            let toMove =  referenceIndex + shift
+            
+            if(referenceIndex > -1){ 
+                if(toMove >= alphabet.length){
+                    toMove = (toMove % (alphabet.length - 1))
+                    strArray[indx] = alphabet[toMove - 1]
+                }
+                else if (toMove < 0){
+                    if((toMove * -1) > alphabet.length){
+                       toMove = (toMove * -1) % (alphabet.length - 1) 
+                       toMove = (alphabet.length) - toMove
+                       strArray[indx] = alphabet[toMove]
+                    }
+                    else{
+                        toMove = (alphabet.length) - (-1 * toMove)
+                        strArray[indx] = alphabet[toMove]
+                    }
+                }
+                else{            
+                 strArray[indx] = alphabet[toMove]
+                }
+            }
+            
+        }
+        let final = strArray.join('')
+        
+        return final
+        
+    }
     return
 }
--- practise/testPractice/caesar.test.js
+++ practise/testPractice/caesar.test.js
@@ -4,7 +4,7 @@ import {caesar} from './caesar.js';
 test('returns undefined if the arguments are not the right type', () => {
     expect(caesar(4,2)).toBe(undefined);
     let obj = {};
-    expect(capitals(obj,'a')).toBe(undefined);
+    expect(caesar(obj,'a')).toBe(undefined);
 
 })
 test('returns undefined if the argument is not given', () => {
@@ -12,24 +12,24 @@ test('returns undefined if the argument is not given', () => {
 })
 
 test('shifts by correct no. of times', () => {
-    expect(caesar('abba',5)).toBe('effe')
+    expect(caesar('abba',5)).toBe('fggf')
     expect(caesar('maupax',2)).toBe('ocwrcz')
     expect(caesar('b',0)).toBe('b')
 })
 
 test('shifts in reverse by correct no. of times', () => {
-    expect(caesar('boby'),-1).toBe('anax')
+    expect(caesar('boby', -1)).toBe('anax')
     expect(caesar('wont',-8)).toBe('ogfl')
 })
 
 test('when shift value exceeds length of alphabet, carried over to the start', () => {
     expect(caesar('xyzw',10)).toBe('hijg')
-    expect(caesar('lab',25)).toBe('mbc')
+    expect(caesar('lab',27)).toBe('mbc')
 })
 
 test('when shift-reverse value exceeds length of alphabet, reverse carry over', () => {
     expect(caesar('hijg',-10)).toBe('xyzw')
-    expect(caesar('mbc',-25)).toBe('lab')
+    expect(caesar('mbc',-27)).toBe('lab')
 })
 
 test('everything in lower case', () => {
@@ -38,7 +38,7 @@ test('everything in lower case', () => {
 })
 
 test('spaces and punctuation marks remain', () => {
-    expect(caesar('aaa !@23'),3).toBe('ddd !@23')
-    expect(caesar('....!'),3).toBe('....!')
+    expect(caesar('aaa !@23',3)).toBe('ddd !@23')
+    expect(caesar('....!',3)).toBe('....!')
 })
 
