99106c2220653cbb8a00f84e78caf007ddef41eb
rxthew <richardtheumaster@gmail.com>
2022-01-29 22:24:05 +0100

    Analyse arrary implementation

--- practise/testPractice/analyseArr.js
+++ practise/testPractice/analyseArr.js
@@ -1,3 +1,44 @@
-export const analyse = function(){
-    return
+import {calc} from './calculator.js'
+
+let sum = calc.sum;
+let divide = calc.divide;
+let maximum = function(a,b){
+    if(a > b){
+        return a
+    }
+    return b
+}
+let minimum = function(a,b){
+    if(a<b){
+        return a
+    }
+    return b
+}
+
+export const analyse = function(thisArr){
+    
+    if(Array.isArray(thisArr)){
+        
+        for(let elem of thisArr){
+            if( typeof elem !== 'number' && typeof elem !== 'bigint'){
+                return false
+
+        }}
+
+        const length = thisArr.length
+        if(length <= 0){return false}
+        const average = divide(thisArr.reduce(sum),length)
+        const max = thisArr.reduce(maximum);
+        const min = thisArr.reduce(minimum)
+
+
+        return {
+            length,
+            average,
+            max,
+            min
+        }
+
+    }
+    return 
 }
--- practise/testPractice/analyseArr.test.js
+++ practise/testPractice/analyseArr.test.js
@@ -26,41 +26,36 @@ test('array is populated with number types', () => {
     let badArray2 = ['',1,3,4];
     let empty = []
 
-    badArray1.forEach(
-        (op) => expect(analyse(op)).toBe(false))
-
-    badArray2.forEach(
-        (op) => expect(analyse(op)).toBe(false))
-    
-    empty.forEach(
-            (op) => expect(analyse(op)).toBe(false))
+    expect(analyse(badArray1)).toBe(false)
+    expect(analyse(badArray2)).toBe(false)
+    expect(analyse(empty)).toBe(false)
 
 })
 
 test('average returned is the correct value', () => {
-    expect(analyse(sample)['average']).toBe(2.5)
+    expect(analyse([1,2,3,4])['average']).toBe(2.5)
     expect(analyse([0,0])['average']).toBe(0)
-    expect(analyse([1,3,-2,3]))['average'].toBe(1.25)
+    expect(analyse([1,3,-2,3])['average']).toBe(1.25)
 
 })
 
 test('min returned is the correct value', () => {
-    expect(analyse(sample)['min']).toBe(1)
+    expect(analyse([1,2,3,4])['min']).toBe(1)
     expect(analyse([0,0])['min']).toBe(0)
-    expect(analyse([1,3,-2,3]))['min'].toBe(-2)
+    expect(analyse([1,3,-2,3])['min']).toBe(-2)
     
 })
 
 test('max returned is the correct value', () => {
-    expect(analyse(sample)['max']).toBe(4)
+    expect(analyse([1,2,3,4])['max']).toBe(4)
     expect(analyse([0,0])['max']).toBe(0)
-    expect(analyse([1,3,-2,3]))['max'].toBe(3)
+    expect(analyse([1,3,-2,3])['max']).toBe(3)
     
 })
 test('length returned is the correct value', () => {
-    expect(analyse(sample)['length']).toBe(4)
+    expect(analyse([1,2,3,4])['length']).toBe(4)
     expect(analyse([0,0])['length']).toBe(2)
-    expect(analyse([1,3,-2,3]))['length'].toBe(4)
+    expect(analyse([1,3,-2,3])['length']).toBe(4)
     
 })
 
