426aba3159f35e59a475e05f5bcf128708dcecc2
rxthew <richardtheumaster@gmail.com>
2022-06-22 22:46:13 +0200

    Make items editable
    No point to finish because rest of the task follows from  previous logic

--- practise/reactPractice/task-app/src/Overview.js
+++ practise/reactPractice/task-app/src/Overview.js
@@ -1,20 +1,59 @@
 import React from 'react'
 
+class Editable extends React.Component {
+    constructor(props){
+        super(props)
+        this.changeMode = this.changeMode.bind(this)
+        this.state = {
+            actual : <li onClick={this.changeMode} key={this.props.chosenKey}>{this.props.content}</li>,
+            mode: 'list',
+            reserve : <input onChange={this.changeMode} key={this.props.chosenKey} placeholder={this.props.content} />
+            
+        }
+        
+    }
+
+    changeMode(){
+        if(this.state.mode === 'list'){
+            this.setState( () => {
+                return {
+                    actual : <li onClick={this.changeMode} key={this.props.chosenKey}>{this.props.content}</li>,
+                    mode : 'inp',
+                    reserve : <input onChange={this.changeMode} key={this.props.chosenKey} placeholder={this.props.content}/>
+                }
+            })
+            return
+        }
+        this.setState( () => {
+            return {
+                actual :  <input onChange={this.changeMode} key={this.props.chosenKey} placeholder={this.props.content}/>,
+                mode :  'list',
+                reserve : <li onClick={this.changeMode} key={this.props.chosenKey}>{this.props.content}</li>
+            
+            }
+        })
+
+    }
+
+    render(){
+        return this.state.actual
+    }
+}
+
+ 
 class Tasks extends React.Component {
     constructor(props){
         super(props)
         this.convertToListItems = this.convertToListItems.bind(this)
-        this.makeEditable = this.makeEditable.bind(this)
-    }
 
-    makeEditable(event){
-        
-    }
+        }
+    
 
     convertToListItems(itemsArray){
-        return itemsArray.map(item => <li onClick={this.makeEditable} key={itemsArray.indexOf(item)}>{item}</li>)
+        return itemsArray.map(item => <Editable chosenKey={itemsArray.indexOf(item)} content={item}/>)
 
     }
+
     render(){
         return <div>
             <ul>
