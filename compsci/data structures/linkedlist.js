92c2a67d09d8182909d62882e7b5e9ea7db88114
rxthew <richardtheumaster@gmail.com>
2022-08-31 17:59:41 +0200

    Tweak value & next so that they need to be called

--- compsci/data structures/linkedlist.js
+++ compsci/data structures/linkedlist.js
@@ -11,15 +11,16 @@ const LinkedList = function(...args){
     const convertNodePair = function(arg,ind,list){
         list[ind] = Node(arg.value)
         if(ind > 0){
-            list[ind - 1].next = list[ind]
+            list[ind - 1].next = () => list[ind]
         }
         
     } 
 
-    let nodes = args.map(arg => Node(arg))
-    nodes.map((arg,ind) => convertNodePair(arg,ind,nodes))
+    let nodes = args.map(arg => Node(() => arg))
+    nodes.map((arg,ind,nodes) => convertNodePair(arg,ind,nodes))
     return nodes[0]
     
 }
 
 
+console.log(LinkedList(1,2,3,4).next().value())
