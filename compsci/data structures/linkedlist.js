d7d43fa3567c2a37777652286aabadcd1998f607
rxthew <richardtheumaster@gmail.com>
2022-08-31 17:50:52 +0200

    initialise basic linked list

--- compsci/data structures/linkedlist.js
+++ compsci/data structures/linkedlist.js
@@ -1,9 +1,25 @@
-const Node = function(value, next){
-    let value = null
-    let next = null
+const Node = function(value=null, next=null){
     
     return {
         value,
         next
     }
 }
+
+const LinkedList = function(...args){
+    
+    const convertNodePair = function(arg,ind,list){
+        list[ind] = Node(arg.value)
+        if(ind > 0){
+            list[ind - 1].next = list[ind]
+        }
+        
+    } 
+
+    let nodes = args.map(arg => Node(arg))
+    nodes.map((arg,ind) => convertNodePair(arg,ind,nodes))
+    return nodes[0]
+    
+}
+
+
