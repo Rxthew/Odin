5d3ec5902d833dcdc380b03437443b3d34c7c474
rxthew <richardtheumaster@gmail.com>
2022-08-31 20:17:59 +0200

    Decouple from array methods

--- compsci/data structures/linkedlist.js
+++ compsci/data structures/linkedlist.js
@@ -1,4 +1,4 @@
-const Node = function(value=null, next=null){
+const Node = function(value=() => null, next=() => null){
     
     return {
         value,
@@ -6,65 +6,145 @@ const Node = function(value=null, next=null){
     }
 }
 
-const LinkedList = function(...args){
-    
-    const _convertNodePair = function(arg,ind,list){
-        list[ind] = Node(arg.value)
-        if(ind > 0){
-            list[ind - 1].next = () => list[ind]
-        }
-        
-    } 
+const LinkedList = function(first,...args){
 
-    let nodes = args.map(arg => Node(() => arg))
-    nodes.map((arg,ind,nodes) => _convertNodePair(arg,ind,nodes))
+    let listSize = 1
+    let headNode = Node(() => first)
+    let tailNode = headNode
+
 
     const append = function(value){
-        let node = Node(value)
-        _convertNodePair(node,nodes.length,nodes)
+        if(listSize <= 0){
+            return LinkedList(value)
+        }
+        const newTail = Node(() => value)
+        tailNode.next = () => newTail
+        tailNode = newTail
+        listSize++
+        return         
     }
 
     const prepend = function(value){
-        const head = nodes[0]
-        const node = Node(() => value, () => head)
-        nodes = [node, ...nodes]
+        if(listSize <= 0){
+            return LinkedList(value)
+        }
+        const currentHead = headNode
+        const newHead = Node(() => value, () => currentHead)
+        headNode = newHead
+        listSize++
+        return
     }
 
     const size = function(){
-        return nodes.length
+        return listSize
     }
 
     const head = function(){
-        return nodes[0]
+        return headNode
     }
 
     const tail = function(){
-        return nodes[nodes.length - 1]
+        return tailNode
     }
 
     const at = function(ind){
-        return nodes[ind]
+        let index = 0
+        if(ind < index){
+            return
+        }
+        let current = headNode
+        if(ind === index){
+            return current
+        }
+        else{
+            while(index < ind){
+                index++
+                current = current.next()
+            }
+            return current
+        }
     }
 
     const pop = function(){
-        return nodes.pop()
+        if(listSize < 1){
+            return
+        }
+        let current = headNode;
+        while(current !== tailNode){
+            if(current.next() === tailNode){
+                current.next = () => null
+                tailNode = current
+                break
+            }
+            else {
+                current = current.next()
+            }
+        }
+        listSize--
+        
     }
 
-    
+    const contains = function(val){
+        let current = headNode
+        while(current !== null){
+            if(current.value() === val){
+                return true
+            }
+            else{
+                current = current.next()
+            }
+        }
+        return false
+    }
+
+    const find = function(val){
+        let current = headNode
+        let count = 0
+        while(current !== null){
+            if(current.value() === val){
+                return count
+            }
+            else{
+                current = current.next()
+                count++
+            }
+        }
+        return null
+
+    }
+
+    const toString = function(){
+        let currentNode = headNode
+        let current = '( ' + headNode.value().toString() + ' )'
+        while(currentNode !== null){
+            currentNode = currentNode.next()
+            if(currentNode === null){return current}
+            current += ' ->  ( ' + currentNode.value().toString() + ' )'
+        }
+        return current
+    }
+
+    if(args.length > 0){
+        args.forEach(arg => append(arg))
+    }
+
+
 
     return {
-        value : () => nodes[0].value(),
-        next  : () => nodes[0].next(),
+        value : () => headNode.value(),
+        next  : () => headNode.next(),
         append,
         prepend,
         size,
         head,
         tail,
+        find,
+        contains,
+        at,
+        pop,
+        toString,
+
     }
     
 }
 
-let bro = LinkedList(1,2,3)
-console.log(bro.value())
-bro.prepend(0)
-console.log(bro.next().next().value())
